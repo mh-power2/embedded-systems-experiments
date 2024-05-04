a8ace9cba145bd177876b028c9eb06ab652df0e6
mh-power2 <113442791+mh-power2@users.noreply.github.com>
2024-05-04 18:45:54 +0300

    Add files via upload

---
+++ expirement4/Master/Debug/LCD_program.d
@@ -0,0 +1,12 @@
+LCD_program.o LCD_program.o: ../LCD_program.c ../STD_TYPES.h \
+  ../BIT_MATH.h ../DIO_interface.h ../LCD_config.h ../LCD_interface.h
+
+../STD_TYPES.h:
+
+../BIT_MATH.h:
+
+../DIO_interface.h:
+
+../LCD_config.h:
+
+../LCD_interface.h:
